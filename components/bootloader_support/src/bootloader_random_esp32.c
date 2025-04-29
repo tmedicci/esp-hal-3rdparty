b04da1b945e51b38d6556a84d38f7ac7145e9ae7
Tiago Medicci <tiago.medicci@espressif.com>
2025-04-29 10:11:29 -0300

    Revert "WIP: Fix MCUboot compatibility problems"
    This reverts commit 8f83d8cde218f944be5d4bcc1010571266c63d16.

--- components/bootloader_support/src/bootloader_random_esp32.c
+++ components/bootloader_support/src/bootloader_random_esp32.c
@@ -13,10 +13,6 @@
 #include "esp_log.h"
 #include "soc/io_mux_reg.h"
 
-#ifdef __NuttX__
-#define BOOTLOADER_BUILD 1
-#endif
-
 #ifndef BOOTLOADER_BUILD
 #include "esp_private/periph_ctrl.h"
 #endif
