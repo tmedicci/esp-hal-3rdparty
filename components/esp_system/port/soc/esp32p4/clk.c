14af372fe0f3ea8a50ece848314ceffb4438d3c2
Tiago Medicci <tiago.medicci@espressif.com>
2025-06-13 16:50:41 -0300

    WIP: Enable running P4 on FPGA

--- components/esp_system/port/soc/esp32p4/clk.c
+++ components/esp_system/port/soc/esp32p4/clk.c
@@ -78,7 +78,7 @@ static void select_rtc_slow_clk(soc_rtc_slow_clk_src_t rtc_slow_clk_src);
 
 static const char *TAG = "clk";
 
-void IRAM_ATTR esp_rtc_init(void)
+__attribute__((weak)) void IRAM_ATTR esp_rtc_init(void)
 {
 #if SOC_PMU_SUPPORTED
     pmu_init();
